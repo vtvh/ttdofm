da744b769b9fd0af7fff71c3f307367a5b6ca7f3
Vu Tran Viet Hai <andreehai1011@gmail.com>
2022-07-19 22:04:38 +0700

    up

--- scripts/tarcur.sh
+++
@@ -1,5 +0,0 @@
-# tar current directory and its subfolders
-base=$(basename $PWD)
-cd ..
-tar -czf $base.tar.gz $base
-cd -
--- scripts/tygia.sh
+++
@@ -1,2 +0,0 @@
-#TODO
-curl https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx | grep USD EUR Yuan
